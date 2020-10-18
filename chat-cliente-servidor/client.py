5c690d95247eca3ce486e642ff5d51c4895d2e3c
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2020-10-18 17:12:36 -0300

    Prototype client.py

---
+++ chat-cliente-servidor/client.py
@@ -0,0 +1,18 @@
+import socket
+import threading
+import sys
+
+# definir aqui funções a serem utilizadas
+
+# abrir o socket a ser utilizado
+
+# conectar o socket ao servidor
+# (se falhar imprimir mensagem de erro idêntica a que tá na especificação do trabalho)
+
+# enviar o nome do cliente para o servidor
+# (se o servidor rejeitar pq já existe, imprimir mensagem de erro como na especificação)
+
+# AO MESMO TEMPO (threads)
+# 1 - ler o comando que o usuário está digitando e enviar quando ele apertar enter
+# 1.1 - CASO USUÁRIO APERTE CTRL+C, PRIMEIRO FECHA A CONEXÃO, IMPRIME "Conexão encerrada." E SAIR DO PROGRAMA. (usar try except, a exceção é KeyboardInterrupt)
+# 2 - imprimir qualquer mensagem que o servidor enviar para esse cliente
