704255fb7863724da2f713404924df057bf532c4
Luiz C <luizdantas.cavalcanti@gmail.com>
2020-10-20 20:44:04 -0300

    Fix CTRL+C not doing anything when accepting connections

--- chat-cliente-servidor/server.py
+++ chat-cliente-servidor/server.py
@@ -1,8 +1,13 @@
 from socket import AF_INET, SOCK_STREAM, socket, gethostbyname, gethostname
 from threading import Thread
-import sys
+from select import select
+from sys import argv
 
 NUM_BYTES = 4096 # ???????
+
+def socket_available():
+    available = select([server_socket],[],[],0.25)[0]
+    return available[0][0] if available else None
 
 def thread_client(connection, address):
     # recebe primeiro comando (o nome de usuario)
@@ -14,17 +19,21 @@ def thread_client(connection, address):
 
 
 
-if name == "__main__":
-
+if __name__ == "__main__":
     try:
+        PORT_NUM = argv[1]
         server_socket = socket(AF_INET,SOCK_STREAM)
-        server_socket.bind(gethostbyname(gethostname()),sys.argv[2]) # tomara que funcione
-        server_socket.listen() # descobrir melhor numero para esse argumento
+        server_socket.bind((gethostbyname(gethostname()),int(PORT_NUM)))
+        server_socket.listen(127)
+
         users_connected = []
+
         while True:
-            connection, address = server_socket.accept()
-            # coloca a conexão na lista de conexões
-            # cria e inicia thread desse cliente que acabou de conectar    # .daemon = True
+            if socket_available():
+                connection, address = server_socket.accept()
+                users_connected.append(connection)
+                # cria e inicia thread desse cliente que acabou de conectar
+                # thread_client.daemon = True
     except KeyboardInterrupt:
         # fecha todas as conexões
-        exit()
+        quit()
