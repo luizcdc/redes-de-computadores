3d442b8c66929b1bf47c5dcde30143a1621e02dd
Alberto Lucas <38790522+AlbertoLucass@users.noreply.github.com>
2020-10-19 21:49:42 -0300

    Quantidade correta de argumentos

--- chat-cliente-servidor/client.py
+++ chat-cliente-servidor/client.py
@@ -16,3 +16,15 @@ import sys
 # 1 - ler o comando que o usuário está digitando e enviar quando ele apertar enter
 # 1.1 - CASO USUÁRIO APERTE CTRL+C, PRIMEIRO FECHA A CONEXÃO, IMPRIME "Conexão encerrada." E SAIR DO PROGRAMA. (usar try except, a exceção é KeyboardInterrupt)
 # 2 - imprimir qualquer mensagem que o servidor enviar para esse cliente
+
+def client():
+	if(len(sys.argv) < 4):
+		print ('Uso: client_chat <CLIENT_NAME> <SERVER_ADDRESS> <SERVER_PORT>')
+		sys.exit()
+
+username = sys.argv[1]  
+host = sys.argv[2]
+port = int(sys.argv[3])
+
+if __name__ == "__main__":
+	sys.exit(client())
