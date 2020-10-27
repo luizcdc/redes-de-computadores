e692716e772e3473c56f72e64f8590d6fbff717c
andreluizsgf <andreluizsgf@gmail.com>
2020-10-27 19:42:40 -0300

    feat: faz funcao send

--- chat-cliente-servidor/server.py
+++ chat-cliente-servidor/server.py
@@ -2,6 +2,7 @@ from socket import AF_INET, SOCK_STREAM, socket, gethostbyname, gethostname
 from threading import Thread
 from select import select
 from sys import argv
+from datetime import datetime
 
 NUM_BYTES = 2000
 users_connected = []
@@ -24,11 +25,9 @@ def remove_connection(connection):
     connection.close()
     return
 
-
 def thread_client(connection, address):
     """Handler para cada cliente que é executado em uma thread própria para cada um."""
     global users_connected
-
     nickname = connection.recv(NUM_BYTES)
     if (nickname in (c[1] for c in users_connected)) or (' ' in nickname):
         # caso o nome de usuario estiver em uso, desconecta, exclui a conexão
@@ -43,13 +42,16 @@ def thread_client(connection, address):
             if x[0] == connection:
                 x[1] = nickname
                 break
-
+        # print(datetime.now().strftime("%H:%M\tAndré Conectado"))
     while True:
-        command = connection.recv(NUM_BYTES)
+        command = connection.recv(NUM_BYTES).decode("utf-8") 
         # identifica o comando e chama a função que executa o comando
         pass
 
-
+def send(connection, message):
+    for(user in users_connected):
+        if(user[0] != connection ):
+            connection.send(message)
 
 if __name__ == "__main__":
     try:
@@ -59,7 +61,6 @@ if __name__ == "__main__":
         server_socket = socket(AF_INET,SOCK_STREAM)
         server_socket.bind((gethostbyname(gethostname()),int(PORT_NUM)))
         server_socket.listen(127)
-
 
         while True:
             if socket_available():
