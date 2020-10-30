c3fea5392d70658fa363a34f90df6a15d8cf5afd
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2020-10-30 11:15:09 -0300

    Apagar comentários antigos e importar constantes

--- chat-cliente-servidor/client.py
+++ chat-cliente-servidor/client.py
@@ -1,28 +1,11 @@
 import socket
 import threading
 import sys
-from server import binary_message_to_string, message_to_binary
-
-# definir aqui funções a serem utilizadas
-
-# abrir o socket a ser utilizado
-
-# conectar o socket ao servidor
-# (se falhar imprimir mensagem de erro idêntica a que tá na especificação do trabalho)
-
-# enviar o nome do cliente para o servidor
-# (se o servidor rejeitar pq já existe, imprimir mensagem de erro como na especificação)
-
-# AO MESMO TEMPO (threads)
-# 1 - ler o comando que o usuário está digitando e enviar quando ele apertar enter
-# 1.1 - CASO USUÁRIO APERTE CTRL+C, PRIMEIRO FECHA A CONEXÃO, IMPRIME "Conexão encerrada." E SAIR DO PROGRAMA. (usar try except, a exceção é KeyboardInterrupt)
-# 2 - imprimir qualquer mensagem que o servidor enviar para esse cliente
+from server import binary_message_to_string, message_to_binary, NUM_BYTES, ENCODING
 
 # AF_INET == ipv4 ------- SOCK_STREAM == TCP
 socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-# conectando ao server
 def send_msg():
     global socket_connection
     try:
@@ -39,8 +22,8 @@ def client():
         USERNAME = sys.argv[1]
         HOST = sys.argv[2]
         PORT = int(sys.argv[3])
-
         try:
+            # conectando ao server
             dest = (HOST, PORT)
             socket_connection.connect(dest)
             socket_connection.sendall(message_to_binary(USERNAME))
@@ -49,12 +32,11 @@ def client():
             thread_send.daemon = True
             thread_send.start()
             while True:
-                msg_recebida = binary_message_to_string(socket_connection.recv(2000))
+                msg_recebida = binary_message_to_string(socket_connection.recv(NUM_BYTES))
                 if(msg_recebida):
                     print(msg_recebida)
                 else:
                     raise ConnectionError('O servidor desconectou.')
-            # send_msg()
 
         except KeyboardInterrupt:
             # TODO: fechar todas as conexões
